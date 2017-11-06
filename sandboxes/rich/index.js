84447b1192458c38b9b13105ecaf37302290faf4
rlaird21 <32648712+rlaird21@users.noreply.github.com>
2017-11-06 14:51:26 -0500

    started working on stop timer

--- sandboxes/rich/index.js
+++ sandboxes/rich/index.js
@@ -7,8 +7,19 @@ res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  // This is what happens when server hears someone clicks a box.
   socket.on('box message', function(msg,color){
     io.emit('box message', msg, color);
+  });
+
+  // This is what happens when servers hears that someone started the game.
+  socket.on('start message', function() {
+    // emit start message to everyone
+    io.emit('start message');
+    // start the timer
+    var timerElment = document.getElementById('timer')
+    // when timer runs out sent stop message to everyone
+    io.emit('stop message');
   });
 });
 
