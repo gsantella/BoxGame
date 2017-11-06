fce8c518f95a7fe1a8dc7f88e6f41336f38002bc
Jonathan Mosel <jmosel38@southhills.edu>
2017-11-06 14:55:44 -0500

    trying to get server to generate color upon user connection

--- sandboxes/jonathan/index.js
+++ sandboxes/jonathan/index.js
@@ -2,13 +2,26 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var colorArray = ["red", "blue", "green", "orange", "yellow"];
+var myColor = colorArray[Math.floor(Math.random() * colorArray.length)];
+
+
 app.get('/', function(req, res){
 res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.on('box message', function(msg,color){
+
+// Gives user the color
+socket.emit('color message', myColor)
+console.log(colorArray.pop(myColor));
+
+  socket.on('box message', function(msg){
+
+
+
     io.emit('box message', msg, color);
+
   });
 });
 
