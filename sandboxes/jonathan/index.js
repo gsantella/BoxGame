8b2e37923155ecc60424fe8d8b3fe8ae5ae117ed
Jonathan Mosel <jmosel38@southhills.edu>
2017-11-07 11:53:58 -0500

    Server Picks Color

--- sandboxes/jonathan/index.js
+++ sandboxes/jonathan/index.js
@@ -2,8 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var colorArray = ["red", "blue", "green", "orange", "yellow"];
-var myColor = colorArray[Math.floor(Math.random() * colorArray.length)];
+var colorArray = ['red', 'blue', 'green', 'orange', 'yellow'];
+
 
 
 app.get('/', function(req, res){
@@ -12,18 +12,25 @@ res.sendFile(__dirname + '/index.html');
 
 io.on('connection', function(socket){
 
-// Gives user the color
-socket.emit('color message', myColor)
-console.log(colorArray.pop(myColor));
+  // Gives user the color
+  var myColor = colorArray.pop();
+  socket.emit('color message', myColor);
 
   socket.on('box message', function(msg){
-
-
-
     io.emit('box message', msg, color);
+  });
 
+  socket.on('color add message', function(color){
+    colorArray.push(color);
+  });
+
+  socket.on('disconnect', function() {
+    var colorArrayOriginal = ['red', 'blue', 'green', 'orange', 'yellow'];
+
+    socket.emit('test', colorArrayOriginal);
   });
 });
+
 
 http.listen(80, function(){
   console.log('listening on *:80');
